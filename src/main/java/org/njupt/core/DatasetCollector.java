31a04b0aff55f11f7e5a70b72919e2dead9bf5c6
dzy_git <1327293674@qq.com>
2023-11-09 23:05:13 +0800

    add Collect all conflicts and merge them in token-level  [2023年11月9日23:05:06]

--- src/main/java/org/njupt/core/DatasetCollector.java
+++ src/main/java/org/njupt/core/DatasetCollector.java
@@ -5,9 +5,12 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.text.similarity.JaccardSimilarity;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.njupt.util.DzyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +19,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class DatasetCollector {
 
@@ -33,29 +34,40 @@ public class DatasetCollector {
 
     public static final String TypeScriptDirectory = "G:/now/2024merge/MergeBERT_Data/fse2022/automated-analysis-data/TypeScript/";
 
+    public static final String JSON = "/json/";
+
     /**
      * Extract conflict tuples from every x_metadata.json file (according to MergeBERT)
-     * @param jsonPath one x_metadata.json file path
+     * @param jsonDirectory one x_metadata.json directory path
      * @return tuples are this file's (A、O、B、R)s
      * @throws IOException IO
      * @throws JSONException JSON
      */
-    public List<List<String>> getTupleFromJson(String jsonPath) throws IOException, JSONException {
-        logger.info("Start Collect Tuples(A O B R) From Only One JSON File : {}", jsonPath);
+    public List<Map<String, String>> getTupleFromJson(String jsonDirectory, String jsonName) throws IOException, JSONException {
+        logger.info("Start Collect Tuples(A O B R) From Only One JSON File : {}", jsonName);
+        List<Map<String, String>> tuples = new ArrayList<>();
 
-        List<List<String>> tuples = new ArrayList<>();
-
-        File file = new File(jsonPath);
+        File file = new File(jsonDirectory + jsonName);
         String content = FileUtils.readFileToString(file, "UTF-8");
         JSONObject jsonObject = new JSONObject(content);
         JSONArray jsonArray = jsonObject.getJSONArray("conflicting_chunks");
-        logger.info("Merged file name is : {}, and it has {} conflicts", jsonObject.get("fname"), jsonArray.length());
+        logger.info("Merged file name is : {}, and it has {} conflicts", jsonName, jsonArray.length());
         for (int i = 0; i < jsonArray.length(); i++){
-            List<String> tuple = new ArrayList<>();
-            tuple.add(jsonArray.getJSONObject(i).get("a_contents").toString());
-            tuple.add(jsonArray.getJSONObject(i).get("base_contents").toString());
-            tuple.add(jsonArray.getJSONObject(i).get("b_contents").toString());
-            tuple.add(jsonArray.getJSONObject(i).get("res_region").toString());
+
+            Map<String, String> tuple = new HashMap<>();
+            tuple.put("a_contents", jsonArray.getJSONObject(i).get("a_contents").toString());
+            tuple.put("o_contents", jsonArray.getJSONObject(i).get("base_contents").toString());
+            tuple.put("b_contents", jsonArray.getJSONObject(i).get("b_contents").toString());
+            tuple.put("res_region", jsonArray.getJSONObject(i).get("res_region").toString());
+            try{
+                tuple.put("res_label", jsonArray.getJSONObject(i).get("label").toString());//may be wrong.
+            }catch (JSONException e){
+                tuple.put("res_label", "null");
+            }
+            tuple.put("file_name", jsonObject.getString("fname"));
+            tuple.put("json_name", jsonName);
+            tuple.put("repo", jsonObject.getString("repo"));
+
             tuples.add(tuple);
         }
         return tuples;
@@ -148,27 +160,26 @@ public class DatasetCollector {
 
     /**
      * 1.Get tuple from Json(getTupleFromJson); 2.Use newTokenizer to tokens; 3. Use tokenListToNewFile to files; 4. Use Diff3 git merge to merged file.
-     * @param jsonPath Json file root path
+     * @param jsonDirectory Json file root path
      * @param jsonName Json file name
      * @throws JSONException JSON
      * @throws IOException IO
      */
-    public void fromTupleToTokenDiff(String jsonPath, String jsonName) throws JSONException, IOException {
-
-        List<List<String>> tuples = getTupleFromJson(jsonPath + jsonName);
+    public void fromTupleToTokenDiff(String jsonDirectory, String jsonName, JSONArray jsonArrayLineLevel, Map<String, Integer> mapCount) throws JSONException, IOException {
+        List<Map<String, String>> tuples = getTupleFromJson(jsonDirectory, jsonName);
         String preName = jsonName.substring(0, jsonName.length() - 13);
 
-        logger.info("Start use Diff3 merge A O B to generate Token-level conflicts:-------------------------------------------------");
+        logger.info("Start use Diff3 merge A O B to generate Token-level conflicts:-------------------------------------------------{}", jsonName);
         for (int i = 0; i < tuples.size(); i++){
-            String aPath = jsonPath + preName + (i + 1) + "_A.txt";
-            String oPath = jsonPath + preName + (i + 1) + "_O.txt";
-            String bPath = jsonPath + preName + (i + 1) + "_B.txt";
-            String mergedPath = jsonPath + preName + (i + 1) + "_merged.txt";
+            String aPath = jsonDirectory + preName + (i + 1) + "_A.txt";
+            String oPath = jsonDirectory + preName + (i + 1) + "_O.txt";
+            String bPath = jsonDirectory + preName + (i + 1) + "_B.txt";
+            String mergedPath = jsonDirectory + preName + (i + 1) + "_merged.txt";
 
             //1.tokenize 2.listToFile 3.turn:A.O.B.R
-            tokenListToNewFile(aPath,newTokenizer(tuples.get(i).get(0)));//A
-            tokenListToNewFile(oPath,newTokenizer(tuples.get(i).get(1)));//O
-            tokenListToNewFile(bPath,newTokenizer(tuples.get(i).get(2)));//B
+            tokenListToNewFile(aPath,newTokenizer(tuples.get(i).get("a_contents")));//A
+            tokenListToNewFile(oPath,newTokenizer(tuples.get(i).get("o_contents")));//O
+            tokenListToNewFile(bPath,newTokenizer(tuples.get(i).get("b_contents")));//B
 
             //Start use Diff3 merge A O B to generate Token-level conflicts
             File a = new File(aPath);
@@ -194,7 +205,7 @@ public class DatasetCollector {
             }
 
             //Restoring conflicting blocks of token-level to row line-level construction
-            String lineMergedPath = jsonPath + preName + (i + 1) + "_lineMerged.txt";
+            String lineMergedPath = jsonDirectory + preName + (i + 1) + "_lineMerged.txt";
             Path path = Paths.get(lineMergedPath);
 
             FileReader fileReader = new FileReader(merged);
@@ -211,144 +222,74 @@ public class DatasetCollector {
                 }
             }
 
+            String tokenMergeResult = fileContext.toString().replace("_NewLine_", "\n");
             try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
                 //write in Line-Merged file
-                writer.write(fileContext.toString().replace("_NewLine_", "\n"));//Restore special token
+                writer.write(tokenMergeResult);//Restore special token
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
             //Delete temp file(A O B)
-//            a.delete();
-//            o.delete();
-//            b.delete();
-        }
-    }
+            a.delete();
+            o.delete();
+            b.delete();
 
-    /**
-     * Customized calculation of perfect match rate. (Note: This calculation is not accurate and can only calculate a perfect match!)
-     * @param ours ours code
-     * @param target target code
-     * @return
-     */
-    private Double perfectMatchRate(String ours, String target){
-        List<String> oursTokens  = new ArrayList<>();
-        List<String> targetTokens  = new ArrayList<>();
+            //Store to JSON file
+            logger.info("Start Collect Line-level Conflict(contain token-level) To Generate JSON File From Only One JSON File : {}", jsonName);
+            mapCount.put("line_allCount", mapCount.getOrDefault("line_allCount", 0) + 1);
 
-        ours = ours.replace("\n", " ");
-        target = target.replace("\n", " ");
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", mapCount.get("line_allCount"));
+            jsonObject.put("id_inFile", i);
+            jsonObject.put("file_name", tuples.get(i).get("file_name"));
+            jsonObject.put("json_name", tuples.get(i).get("json_name"));
+            jsonObject.put("repo", tuples.get(i).get("repo"));
+            jsonObject.put("a_contents", tuples.get(i).get("a_contents"));
+            jsonObject.put("o_contents", tuples.get(i).get("o_contents"));
+            jsonObject.put("b_contents", tuples.get(i).get("b_contents"));
+            jsonObject.put("res_region", tuples.get(i).get("res_region"));
+            jsonObject.put("res_label", tuples.get(i).get("res_label"));
 
-        //USE JavaParser tokenize code.
-        JavaParser javaParser = new JavaParser(new ParserConfiguration().setSymbolResolver(new JavaSymbolSolver(new ReflectionTypeSolver())));
-        Optional<CompilationUnit> compilationUnitOurs = javaParser.parse(ours).getResult();
-        Optional<CompilationUnit> compilationUnitTarget = javaParser.parse(target).getResult();
-        if (compilationUnitOurs.isPresent()) {
-            TokenRange tokenRange = compilationUnitOurs.get().getTokenRange().get();
-            tokenRange.forEach(token -> {
-                if (!Objects.equals(token.getText().replaceAll("\\s*",""), "")){
-                    oursTokens.add(token.getText());
-                }
-            });
-        } else {
-            logger.error("Code not parsed correctly! here:\n{}", ours);
-        }if (compilationUnitTarget.isPresent()) {
-            TokenRange tokenRange = compilationUnitTarget.get().getTokenRange().get();
-            tokenRange.forEach(token -> {
-                if (!Objects.equals(token.getText().replaceAll("\\s*",""), "")){
-                    targetTokens.add(token.getText());
-                }
-            });
-        } else {
-            logger.error("Code not parsed correctly! here:\n{}", target);
-        }
-
-        //make them change to be same length.
-        if (oursTokens.size() < targetTokens.size()){
-            return (double) oursTokens.size() / targetTokens.size();//Note: this is false; It shouldn't be calculated like this
-        }else if (oursTokens.size() > targetTokens.size()){
-            return (double) targetTokens.size() / oursTokens.size();//Note: this is false; It shouldn't be calculated like this
-        }else {
-            int count = 0;
-            for (int i = 0; i < oursTokens.size(); i++){
-                if (Objects.equals(oursTokens.get(i), targetTokens.get(i))) count++;
+            jsonObject.put("can_token_level", true);//先默认全部都适合token-level
+            if (!tokenMergeResult.contains("<<<<<<<")){
+                mapCount.put("tokenMerge_allCount", mapCount.getOrDefault("tokenMerge_allCount", 0) + 1);
+                jsonObject.put("can_merge_succeed", true);
+            }else {
+                jsonObject.put("can_merge_succeed", false);
             }
-            return (double) count / oursTokens.size() * 100;
+            Double matchRate = DzyUtils.perfectMatchRate(tokenMergeResult, tuples.get(i).get("res_region"));
+            if (matchRate == 100) mapCount.put("tokenMerge_correct", mapCount.getOrDefault("tokenMerge_correct", 0) + 1);
+            jsonObject.put("match_rate", matchRate);
+            jsonObject.put("token_level_result", tokenMergeResult);
+            jsonObject.put("key_information", " ");
+            jsonArrayLineLevel.put(jsonObject);
         }
-    }
-
-    /**
-     * Official Code perfect matching rate.
-     */
-    private Double officialPerfectMatchRate(String ours, String target){
-
-        return 100.00;
     }
 
     /**
      * Merge token-level conflicts across the entire directory.
      * @param directory json directory
      */
-    public void allTuplesToTokenDiff(String directory) throws JSONException, IOException {
+    public void allTuplesToTokenDiff(String directory, String jsonName) throws JSONException, IOException {
+        JSONArray jsonArray = new JSONArray();
+        Map<String, Integer> map = new HashMap<>();
         File files = new File(directory);
         String[] list = files.list();
         for (String fileName : list){
             if (fileName.endsWith(".json")){
-                System.out.println(fileName);
-                fromTupleToTokenDiff(directory, fileName);
+                fromTupleToTokenDiff(directory, fileName, jsonArray, map);
             }
         }
+        //Write in file.
+        DzyUtils.stringToBuildFile(directory + JSON + jsonName, jsonArray.toString());
+        logger.info("Statistical results of data:\n{}", map);
     }
-
-
 
     public static void main(String[] args) throws IOException, JSONException {
         DatasetCollector collector = new DatasetCollector();
         //collector.getConflictFromJson("G:\\now\\2024merge\\ChatGPTResearch\\exampleData\\acceptA\\100004_metadata.json");
-
-        String dir = "G:\\now\\2024merge\\ChatGPTResearch\\exampleData\\acceptA\\";
-        String dirB = "G:\\now\\2024merge\\ChatGPTResearch\\exampleData\\acceptB\\";
-        String dirBA = "G:\\now\\2024merge\\ChatGPTResearch\\exampleData\\acceptBA\\";
-//        collector.fromTupleToTokenDiff(dir, "100004_metadata.json");
-//        collector.fromTupleToTokenDiff(dir, "100034_metadata.json");
-//        collector.fromTupleToTokenDiff(dirB, "106855_metadata.json");
-//        collector.fromTupleToTokenDiff(dirBA, "101216_metadata.json");
-//        collector.fromTupleToTokenDiff(dirBA, "140328_metadata.json");
-//        System.out.println(collector.perfectMatchRate("\n" +
-//                " private final static IngestServices ingestServices = IngestServices . getInstance ( ) ; \n" +
-//                " private final static Logger logger = ingestServices . getLogger ( SQLiteReader . class . getName ( ) ) ; \n" +
-//                " \n" +
-//                " ", "    public final static IngestServices ingestServices = IngestServices.getInstance();\n    private final static Logger logger = ingestServices.getLogger(SQLiteReader.class.getName());\n    \n"));
-
-        collector.allTuplesToTokenDiff("G:/now/2024merge/ChatGPTResearch/exampleData/acceptB/");
-
-
-
-//        System.out.println(collector.tokenize("/**\n" +
-//                "     * Select an actor in a Human task in the list of Process Actor\n" +
-//                "     *"));
-//        System.out.println(collector.newTokenizer("/**\n" +
-//                "     * Select an actor in a Human task in the list of Process Actor\n" +
-//                "     *"));
-
-
-        /**
-         * 演示 1 tokenize 与 newTokenizer 区别
-         */
-//        String codeLines = "Platform.runLater(() -> {\n" +
-//                "            //if there is an existing prompt or progressdialog,...\n" +
-//                "            if (promptDialogManager.bringCurrentDialogToFront()) {\n" +
-//                "                //... just show that\n" +
-//                "            } else {\n" +
-//                "\n" +
-//                "                if ( //confirm timeline during ingest\n" +
-//                "                        IngestManager.getInstance().isIngestRunning()\n" +
-//                "                        && promptDialogManager.confirmDuringIngest() == false) {\n" +
-//                "                    return;  //if they cancel, do nothing.\n" +
-//                "                }";
-//        System.out.println(collector.tokenize(codeLines));
-//        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-//        System.out.println(collector.newTokenizer(codeLines));
-
+        collector.allTuplesToTokenDiff(JavaDirectory, "java.json");
 
     }
 
